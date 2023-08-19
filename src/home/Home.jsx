e3fa94572657d5e6dd07987fa71e2172f65e2a0b
Raju1-Ahmed <robiussaniraju@gmail.com>
2023-08-19 18:59:23 +0600

    tab list is done now working for responsive al device

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,28 +2,33 @@
 import { useEffect } from 'react';
 import './style.scss'
 
-const Home = () => {
+const Home = () => {    
     useEffect(() => {
-        // Get references to the elements
         const tabs = document.querySelectorAll('.tab');
         const tabContents = document.querySelectorAll('.tab-content');
 
         tabs.forEach(tab => {
             tab.addEventListener('click', () => {
                 const tabId = tab.getAttribute('data-tab');
-
-                tabs.forEach(t => t.classList.remove('active'));
+                const selectedTabContent = document.querySelector(`.tab-content[data-tab="${tabId}"]`);
+                
+                tabs.forEach(t => {
+                    t.classList.remove('active');
+                });
                 tab.classList.add('active');
 
                 tabContents.forEach(content => {
-                    content.classList.remove('active');
-                    if (content.id === tabId) {
+                    if (content === selectedTabContent) {
                         content.classList.add('active');
+                    } else {
+                        content.classList.remove('active');
                     }
                 });
             });
         });
-
+      
+     
+        // Here's a vanilla Javascript implementation for the search button
         const searchButton = document.getElementById('searchButton');
         const searchBar = document.getElementById('searchBar');
         const closeBtn = document.getElementById('close');
@@ -31,13 +36,17 @@ const Home = () => {
         searchButton.addEventListener('click', function () {
             tabWrap.style.display = 'none';
             searchBar.style.display = 'block';
+
         });
 
         closeBtn.addEventListener('click', function () {
             searchBar.style.display = 'none';
             tabWrap.style.display = 'flex';
         });
-    }, []);     
+
+
+
+    }, []);
     return (
         <div className='menu-wrap'>
             <nav className="navbar">
@@ -63,22 +72,22 @@ const Home = () => {
             </nav>
             <div className="Tab-wrap" id='tabWrap'>
                 <ul>
-                    <li className="tab active" data-tab="tab1 tab">
+                    <li className="tab " data-tab="tab1">
                         <img width="25" height="25" src="https://img.icons8.com/material-outlined/24/FFFFFF/monitor.png" alt="monitor" />
                         pc <img width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/expand-arrow--v1.png" alt="expand-arrow--v1" />
                     </li>
 
-                    <li className="tab active" data-tab="tab2">
+                    <li className="tab " data-tab="tab2">
                         <img width="25" height="25" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/play-station.png" alt="play-station" />
                         Playstation <img width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/expand-arrow--v1.png" alt="expand-arrow--v1" />
                     </li>
 
-                    <li className="tab active" data-tab="tab3">
+                    <li className="tab " data-tab="tab3">
                         <img width="25" height="25" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/xbox.png" alt="xbox" />
                         Xbox <img width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/expand-arrow--v1.png" alt="expand-arrow--v1" />
                     </li>
 
-                    <li className="tab active" data-tab="tab4 ">
+                    <li className="tab " data-tab="tab4">
                         <img width="25" height="25" src="https://img.icons8.com/ios-filled/50/FFFFFF/nintendo-switch-logo.png" alt="nintendo-switch-logo" />
                         Nintendo <img width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/expand-arrow--v1.png" alt="expand-arrow--v1" />
                     </li>
@@ -100,40 +109,47 @@ const Home = () => {
                 </span>
             </div>
 
-            <div  className='platforms ' data-tab="tab4 tab-content " id="tab1" >
-                <div className='view'>
-                    <h2>Platforms</h2>
-                    <a href="">View All</a>
-                </div>
-                <div className='products'>
-                    <div className='product'>
-                        <span className='imageShape'>
+            <div>
+                <div className="platforms tab-content" data-tab="tab1">
+                    <div className='view'>
+                        <h2>Platforms</h2>
+                        <a href="">View All</a>
+                    </div>
+                    <div className='products'>
+                        <div className='product'>
+                            <span className='imageShape'>
+                                <img width="30" height="30" src="https://img.icons8.com/color/48/youtube-live.png" alt="youtube-live" />
+                            </span>
+                            <h3>Steam</h3>
+                        </div>
+
+                        <div className='product'>
                             <img width="30" height="30" src="https://img.icons8.com/color/48/youtube-live.png" alt="youtube-live" />
-                        </span>
-                        <h3>Steam</h3>
-                    </div>
+                            <h3>Steam</h3>
+                        </div>
 
-                    <div className='product'>
-                        <img width="30" height="30" src="https://img.icons8.com/color/48/youtube-live.png" alt="youtube-live" />
-                        <h3>Steam</h3>
-                    </div>
+                        <div className='product'>
+                            <img width="30" height="30" src="https://img.icons8.com/color/48/youtube-live.png" alt="youtube-live" />
+                            <h3>Steam</h3>
+                        </div>
 
-                    <div className='product'>
-                        <img width="30" height="30" src="https://img.icons8.com/color/48/youtube-live.png" alt="youtube-live" />
-                        <h3>Steam</h3>
-                    </div>
+                        <div className='product'>
+                            <img width="30" height="30" src="https://img.icons8.com/color/48/youtube-live.png" alt="youtube-live" />
+                            <h3>Steam</h3>
+                        </div>
 
-                    <div className='product'>
-                        <img width="30" height="30" src="https://img.icons8.com/color/48/youtube-live.png" alt="youtube-live" />
-                        <h3>Steam</h3>
-                    </div>
-
-                    <div className='product'>
-                        <img width="30" height="30" src="https://img.icons8.com/color/48/youtube-live.png" alt="youtube-live" />
-                        <h3>Steam</h3>
+                        <div className='product'>
+                            <img width="30" height="30" src="https://img.icons8.com/color/48/youtube-live.png" alt="youtube-live" />
+                            <h3>Steam</h3>
+                        </div>
                     </div>
                 </div>
+                <div className="platforms tab-content " data-tab="tab2">This is two tab</div>
+                <div className="platforms tab-content " data-tab="tab3">This is three tab</div>
+                <div className="platforms tab-content " data-tab="tab4">This is four tab</div>
             </div>
+
+            
         </div>
     );
 };
